e02f35c056648ea8e163b0c8bcf9db4c8a237b1d
Tharikaa Loganathan <tharikaals2005@gmail.com>
2025-12-24 13:17:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (49.92%) - LeetSync

--- 504-base-7/base-7.java
+++ 504-base-7/base-7.java
@@ -1,19 +1,20 @@
 class Solution {
     public String convertToBase7(int num) {
-        int sum=0;
-        if(num==0) return "0";
-       StringBuffer sb=new StringBuffer();
-       boolean n=false;
-       if(num<0)
-        n=true;
-        num=Math.abs(num);
-       while(num!=0)
-       {
-        sb.append(num%7);
-        num/=7;
-       } 
-       sb.reverse();
-       sb.toString();
-       return (n?"-"+sb:sb).toString();
+    //     int sum=0;
+    //     if(num==0) return "0";
+    //    StringBuffer sb=new StringBuffer();
+    //    boolean n=false;
+    //    if(num<0)
+    //     n=true;
+    //     num=Math.abs(num);
+    //    while(num!=0)
+    //    {
+    //     sb.append(num%7);
+    //     num/=7;
+    //    } 
+    //    sb.reverse();
+    //    sb.toString();
+    //    return (n?"-"+sb:sb).toString();
+    return Integer.toString(num,7);
     }
 }
