a3b72cafb83688a5440f97b6fd310e371bb4501f
Tharikaa Loganathan <tharikaals2005@gmail.com>
2025-12-28 18:25:54 +0530

    Time: 3 ms (1.15%) | Memory: 46.6 MB (12.09%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -21,7 +21,7 @@ class Solution {
         }
         for(int i=0;i<normal.size();i++)
         {
-            if(i%2==0)
+            if((i&1)==0)
             {
                 even.add(normal.get(i));
             }
