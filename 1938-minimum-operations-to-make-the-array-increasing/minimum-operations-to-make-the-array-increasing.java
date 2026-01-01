6e528a82ad4e9a5136098ee20a3ca63c50dbeb30
Tharikaa Loganathan <tharikaals2005@gmail.com>
2026-01-01 19:32:39 +0530

    Time: 4 ms (77.13%) | Memory: 47.2 MB (20.70%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
@@ -3,10 +3,11 @@ class Solution {
         int c=0;
         for(int i=1;i<nums.length;i++)
         {
-            while(nums[i]<=nums[i-1])
+            if(nums[i-1]>=nums[i])
             {
-                nums[i]++;
-                c++;
+                int add=nums[i-1]-nums[i]+1;
+                nums[i]+=add;
+                c+=add;
             }
         }
         return c;
