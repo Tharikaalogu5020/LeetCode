d178a970d911e96c78009b4043211033c4ce6610
Tharikaa Loganathan <tharikaals2005@gmail.com>
2026-01-01 19:05:48 +0530

    Time: 224 ms (9.88%) | Memory: 47.4 MB (8.80%) - LeetSync

---
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int c=0;
+        for(int i=1;i<nums.length;i++)
+        {
+            while(nums[i]<=nums[i-1])
+            {
+                nums[i]++;
+                c++;
+            }
+        }
+        return c;
+    }
+}
