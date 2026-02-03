6109eb703500a3de6f2b9091640ca3c3ae8bdd74
Tharikaa Loganathan <tharikaals2005@gmail.com>
2026-02-03 20:05:41 +0530

    Time: 1 ms (95.95%) | Memory: 47.4 MB (55.12%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.java
+++ 203-remove-linked-list-elements/remove-linked-list-elements.java
@@ -10,22 +10,21 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        ListNode temp=head;
-        List<Integer> l=new ArrayList<>();
-        while(temp!=null)
-        {
-            if(temp.val!=val)
-            l.add(temp.val);
-            temp=temp.next;
-        }
         ListNode dummy=new ListNode(0);
-        ListNode t=dummy;
-        for(int i:l)
+         dummy.next=head;
+      ListNode temp=dummy;
+        while(temp.next!=null)
         {
-            t.next=new ListNode(i);
-        t=t.next;
+            if(temp.next.val==val)
+            {
+               temp.next=temp.next.next;
+            }
+            else{
+                
+                temp=temp.next;
+            }
+            
         }
         return dummy.next;
-
     }
 }
