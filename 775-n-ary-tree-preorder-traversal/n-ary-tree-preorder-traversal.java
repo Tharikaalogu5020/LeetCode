6dc45bf008eccb93f8550f610be83c6f30266121
Tharikaa Loganathan <tharikaals2005@gmail.com>
2026-06-25 10:49:10 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (92.93%) - LeetSync

---
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.java
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    ArrayList<Integer> arr=new ArrayList<>();
+    public List<Integer> preorder(Node root) {
+        
+        pre(root,arr);
+        return arr;
+    }
+     public static void pre(Node root,ArrayList<Integer> arr)
+     {
+        if(root==null)
+        {
+            return;
+        }
+        arr.add(root.val);
+        for(Node child:root.children)
+        {
+            pre(child,arr);
+        }
+     }
+
+}
