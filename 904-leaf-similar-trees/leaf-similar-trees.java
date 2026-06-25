5f0e595b94125cd6875410499c5c4fc3c8f52c95
Tharikaa Loganathan <tharikaals2005@gmail.com>
2026-06-25 15:15:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (16.67%) - LeetSync

---
+++ 904-leaf-similar-trees/leaf-similar-trees.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        ArrayList<Integer> arr=new ArrayList<>();
+        ArrayList<Integer> arr1=new ArrayList<>();
+        ArrayList<Integer> ans1=firsttree(root1,arr); 
+        ArrayList<Integer> ans2= secondtree(root2,arr1);
+       return ans1.equals(ans2);
+    }
+    public ArrayList<Integer> firsttree(TreeNode root1,ArrayList<Integer> arr)
+    {
+        if(root1==null)
+        {
+            return arr;
+        }
+        if(root1.left==null && root1.right==null)
+        {
+            arr.add(root1.val);
+        }
+        firsttree(root1.left,arr);
+        firsttree(root1.right,arr);
+        return arr;
+    }
+    public ArrayList<Integer> secondtree(TreeNode root2,ArrayList<Integer> arr1)
+    {
+        if(root2==null)
+        {
+            return arr1;
+        }
+        if(root2.left==null && root2.right==null)
+        {
+            arr1.add(root2.val);
+        }
+        secondtree(root2.left,arr1);
+        secondtree(root2.right,arr1);
+        return arr1;
+    }
+    
+}
