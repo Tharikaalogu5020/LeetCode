93b1457715aa5d1a24903112f7cc2402e599e03c
Tharikaa Loganathan <tharikaals2005@gmail.com>
2026-06-29 14:17:28 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (72.97%) - LeetSync

---
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode ans=null;
+    public TreeNode searchBST(TreeNode root, int val) {
+        find(root,val);
+        return ans;
+    }
+    public void find(TreeNode root,int val)
+    {
+        if(root==null)
+        {
+            return ;
+        }
+        
+        if(root.val==val )
+        {
+            ans=root;
+            
+        }
+        
+         searchBST(root.left,val);
+        
+          searchBST(root.right,val);
+        
+        
+    }
+    
+}
