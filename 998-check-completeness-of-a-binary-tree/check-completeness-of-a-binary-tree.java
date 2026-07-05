3446c601101bd98daae2df48f5c3d5787a24bb6a
Tharikaa Loganathan <tharikaals2005@gmail.com>
2026-07-05 15:56:04 +0530

    Time: 1 ms (83.00%) | Memory: 43.8 MB (74.62%) - LeetSync

---
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+        public boolean isCompleteTree(TreeNode root) {
+
+                Queue<TreeNode> queue = new LinkedList<>();
+                        queue.offer(root);
+
+                                boolean nullFound = false;
+
+                                        while (!queue.isEmpty()) {
+
+                                                    TreeNode curr = queue.poll();
+
+                                                                if (curr == null) {
+                                                                                nullFound = true;
+                                                                                            } else {
+
+                                                                                                            if (nullFound)
+                                                                                                                                return false;
+
+                                                                                                                                                queue.offer(curr.left);
+                                                                                                                                                                queue.offer(curr.right);
+                                                                                                                                                                            }
+                                                                                                                                                                                    }
+
+                                                                                                                                                                                            return true;
+                                                                                                                                                                                                }
+                                                                                                                                                                                                }
