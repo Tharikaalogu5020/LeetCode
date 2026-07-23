e752f20d77e073e3652f9ead1085a7294114196f
Tharikaa Loganathan <tharikaals2005@gmail.com>
2026-07-23 12:46:16 +0530

    Time: 2 ms (93.97%) | Memory: 65.1 MB (70.09%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,27 +1,22 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] pre=new int[nums.length];
-        int[] post=new int[nums.length];
-        int res[]=new int[nums.length];
-        
-        
-            //pre
-            pre[0]=1;
-            for(int j=1;j<nums.length;j++)
-            {
-                pre[j]=pre[j-1]*nums[j-1];
-            }
-            //post
-            post[nums.length-1]=1;
-            for(int k=nums.length-2;k>=0;k--)
-            {
-                post[k]=nums[k+1]*post[k+1];
-            } 
-            for(int i=0;i<nums.length;i++)
-            {
-                res[i]=pre[i]*post[i];
-            }
-        
+        int[] arr1=new int[nums.length];
+        int[] arr2=new int[nums.length];
+        int[] res=new int[nums.length];
+        arr1[0]=1;
+        for(int i=1;i<nums.length;i++)
+        {
+            arr1[i]=arr1[i-1]*nums[i-1];
+        }
+        arr2[nums.length-1]=1;
+        for(int j=nums.length-2;j>=0;j--)
+        {
+            arr2[j]=arr2[j+1]*nums[j+1];
+        }
+        for(int i=0;i<arr1.length;i++)
+        {
+            res[i]=arr1[i]*arr2[i];
+        }
         return res;
-     }
+    }
 }
