190115cfa2160b2a3a7dae17f057f7d1073e400c
Tharikaa Loganathan <tharikaals2005@gmail.com>
2026-07-24 20:28:52 +0530

    Time: 5 ms (4.98%) | Memory: 46.4 MB (5.35%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,15 +1,30 @@
+import java.util.*;
+
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        for(int i=0;i<arr.length;i++)
-        {
-            int miss=arr[i]-(i+1);
-            if(miss>=k)
-            {
-                return i+k;
-            }
+
+        HashSet<Integer> s = new HashSet<>();
+
+        // Store all array elements
+        for(int n : arr) {
+            s.add(n);
         }
-        int totalmiss=arr[arr.length-1]-arr.length;
-        int remain=k-totalmiss;
-        return arr[arr.length-1]+remain;
+
+        ArrayList<Integer> res = new ArrayList<>();
+
+        int i = 1;
+
+        // Find missing positive numbers until we get k numbers
+        while(res.size() < k) {
+
+            if(!s.contains(i)) {
+                res.add(i);
+            }
+
+            i++;
+        }
+
+        // k-th missing number (index k-1)
+        return res.get(k - 1);
     }
 }
